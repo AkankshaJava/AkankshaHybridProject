e6af303977ae889ba5815c72af6e34c497475807
AkankshaJava <akankshavyasjava@gmail.com>
2022-11-10 10:55:05 +0530

    chages

--- src/Learn.java
+++ src/Learn.java
@@ -1,8 +1,9 @@
 
 public class Learn {
 	public static void main(String[] args) {
-		System.out.println("1234");
+		System.out.println("1234aaradhy");
 		System.out.println("45aaradhy67");
+		System.out.println("change done");
 	}
 
 }
